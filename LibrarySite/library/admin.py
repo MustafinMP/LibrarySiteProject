541d7955fd059308ae046ef66991b03b8c28fdf1
Mustafin Maksim <mustafinmp@yandex.ru>
2022-03-29 17:31:06 +0500

    Start clean-up code
    Version 1.0.01

--- LibrarySite/library/admin.py
+++ LibrarySite/library/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre, Status, Textbook, TextbookInstance, PublishingHouse
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 
-
-# admin.site.register(Author)
-# admin.site.register(Book)
-# admin.site.register(BookInstance)
-# admin.site.register(Genre)
-# admin.site.register(Status)
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
@@ -25,18 +17,13 @@ class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'id', 'status', 'borrower')
 
 
-@admin.register(Textbook)
-class TextbookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'display_author', 'publishing_house')
-
-
-@admin.register(TextbookInstance)
-class TextbookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('textbook', 'status', 'borrower')
-
-
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PublishingHouse)
+class PublishingHouseAdmin(admin.ModelAdmin):
     pass
 
 
@@ -45,6 +32,11 @@ class StatusAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(PublishingHouse)
-class PublishingHouseAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Textbook)
+class TextbookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_author', 'publishing_house')
+
+
+@admin.register(TextbookInstance)
+class TextbookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('textbook', 'status', 'borrower')
--- LibrarySite/library/forms.py
+++ LibrarySite/library/forms.py
@@ -1,37 +1,30 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(
-        label='логин',
-        help_text='''Введите свой логин'''
-    )
-    name = forms.CharField(
-        label='Имя:',
-        help_text='Введите Ваше имя'
-    )
-    last_name = forms.CharField(
-        label='Фамилия:',
-        help_text='Введите Вашу фамилию'
-    )
-    e_mail = forms.EmailField(
-        label='Email:',
-        help_text='Введите Вашу электронную почту'
-    )
-    password = forms.CharField(
-        label='Пароль:',
-        widget=forms.PasswordInput,
-        help_text='Введите пароль'
-    )
+    username = forms.CharField(label='Логин',
+                               help_text='''Введите свой логин'''
+                               )
+    name = forms.CharField(label='Имя:',
+                           help_text='Введите Ваше имя'
+                           )
+    last_name = forms.CharField(label='Фамилия:',
+                                help_text='Введите Вашу фамилию'
+                                )
+    e_mail = forms.EmailField(label='Email:',
+                              help_text='Введите Вашу электронную почту'
+                              )
+    password = forms.CharField(label='Пароль:',
+                               widget=forms.PasswordInput,
+                               help_text='Введите пароль'
+                               )
 
 
 class LoginForm(forms.Form):
-# class LoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label='Введите имя'
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput,
-        label='Введите пароль'
-    )
+    username = forms.CharField(label='Логин',
+                               help_text='Введите свой логин'
+                               )
+    password = forms.CharField(widget=forms.PasswordInput,
+                               label='Пароль',
+                               help_text='Введите пароль'
+                               )
